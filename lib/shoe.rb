6689da98976ff621045584160d96e84b0e955b4b
Victor Avila <avilavictor1570@gmail.com>
2020-07-23 03:25:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -31,6 +31,14 @@ class Shoe
     @material
   end
   
-    
-    
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble
+    puts ""
 end
