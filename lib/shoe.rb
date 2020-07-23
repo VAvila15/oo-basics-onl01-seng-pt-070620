0baea7e2c38e98903e464fc53374b73ec89ad911
Victor Avila <avilavictor1570@gmail.com>
2020-07-23 03:21:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,9 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-  def initalize(new)
-    @new = new
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
   end
 end
